468635e07d93a9d4f72838ee98dc603e16f0225a
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-10 22:39:00 +0530

    [Ritesh]add : Display Ten Natural Number Using For Loops

---
+++ ForLoops/NaturalNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    public class NaturalNumber
+    {
+        public void Number()
+        {
+            Console.WriteLine("First Ten Natural Number is :");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);   
+            }
+        }
+    }
+}
---
+++ ForLoops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to For Loop Exercies");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1 : Display First 10 Natyral Number");
+                Console.WriteLine("Enter A Option");
+                int Option = Convert.ToInt32(Console.ReadLine());
+                switch(Option)
+                {
+                    case 1:
+                        NaturalNumber no = new NaturalNumber();
+                        no.Number();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
